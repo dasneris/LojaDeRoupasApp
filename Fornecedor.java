2aeb29987d96248f186a87eef6b995a1a47e4755
Felipe <felipealfaya90@gmail.com>
2023-12-07 09:42:36 -0300

    Completed DB String and creation of a new table supplier for products. Added a view in DB for products with suppliers.

---
+++ Fornecedor.java
@@ -0,0 +1,58 @@
+import java.math.BigDecimal;
+
+public class Fornecedor {
+    private int id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Fornecedor() {
+    }
+
+    public Fornecedor(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", Nome: " + nome +
+                ", Endereço: " + endereco +
+                ", Telefone: " + telefone;
+    }
+}
